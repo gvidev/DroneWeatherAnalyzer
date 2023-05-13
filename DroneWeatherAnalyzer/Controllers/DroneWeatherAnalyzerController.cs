9d8ebb6cb367c2269bf7c104f7da3b77de49475e
Georgi Videv <104200628+gvidev@users.noreply.github.com>
2023-05-13 16:23:41 +0300

    finishing
    I have made view and both post and get methods working. Maybe I can make some refactoring of the code.

--- DroneWeatherAnalyzer/Controllers/DroneWeatherAnalyzerController.cs
+++ DroneWeatherAnalyzer/Controllers/DroneWeatherAnalyzerController.cs
@@ -9,6 +9,7 @@ using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace DroneWeatherAnalyzer.Controllers
 {
@@ -17,24 +18,35 @@ namespace DroneWeatherAnalyzer.Controllers
 
         private const string _ApiKey = "c5dc795eb4a7c047134466ea058da319";
 
+
         // GET: QuotesAPI
-        [HttpGet ]
+        [HttpGet]
         public ActionResult DroneWeatherAnalyzer()
         {
+           
+
+            Dictionary<string, double> dronesWindResist = DictionaryInitializer();
+            List<SelectListItem> droneList = dronesWindResist.Select(d => new SelectListItem
+            {
+                Text = d.Key
+            }).ToList();
+            ViewBag.Drones = droneList;
+
             //this retrieve current country name
             string countryName = RegionInfo.CurrentRegion.DisplayName;
             //and then with free api gets the Capital of the current country
             string capitalName = FetchCapitalOfCountry(countryName);
 
-
             List<string> geolocation = FetchGeolocation(capitalName);
-            Dictionary<string, double> dronesWindResist = DictionaryInitializer();
-
             if (!geolocation.Any())
             {
-                ViewData["error"] =
+                ViewData["alert"] =
                     "Please be sure that the name of the city that you insert is valid and try again!";
+                ViewData["alertLevel"] = "caution";
+                return View();
             };
+
+            
 
             var url = $"https://api.openweathermap.org/data/2.5/weather?lat={geolocation[0]}&lon={geolocation[1]}&units=metric&APPID={_ApiKey}";
             var client = new WebClient();
@@ -44,8 +56,8 @@ namespace DroneWeatherAnalyzer.Controllers
             JObject data = JObject.Parse(body);
             int mainTempInCelsius = (int)(data["main"]["temp"]);
             double visibilityInKm = (double)data["visibility"] / 1000;
-            // double windSpeed = (double)data["wind"]["speed"];
-            double windSpeed = 15;
+            double windSpeed = (double)data["wind"]["speed"];
+            // double windSpeed = 15;
 
 
             ViewData["temp"] = mainTempInCelsius;
@@ -57,46 +69,101 @@ namespace DroneWeatherAnalyzer.Controllers
             ViewData["visibility"] = visibilityInKm;
             ViewData["atmPressure"] = data["main"]["pressure"];
 
-            var temp = dronesWindResist["DJI Mini 2 SE"];
-
-            ViewData["alert"] = (temp <= windSpeed)
-                ?
-                 $"Fly with caution! Your drone has wind resist of" +
-                    $" {temp} m/s but the wind in the region is {windSpeed} m/s"
-
-                    : "Flying your drone is completely safe!";
-
 
             return View();
         }
 
 
         [HttpPost]
-        public ActionResult DroneWeatherAnalyzer(string city, string droneModel)
+        public ActionResult DroneWeatherAnalyzer(string city, string selectedDroneModel)
         {
+
+            Dictionary<string, double> dronesWindResist = DictionaryInitializer();
+            List<SelectListItem> droneList = dronesWindResist.Select(d => new SelectListItem
+            {
+                Text = d.Key
+            }).ToList();
+            ViewBag.Drones = droneList;
+
+            List<string> geolocation = FetchGeolocation(city);
+            if (!geolocation.Any())
+            {
+                ViewData["alert"] =
+                    "Please be sure that the name of the city that you insert is valid and try again!";
+                ViewData["alertLevel"] = "caution";
+                return View();
+            };
+
+ 
+            
+
+
+            var url = $"https://api.openweathermap.org/data/2.5/weather?lat={geolocation[0]}&lon={geolocation[1]}&units=metric&APPID={_ApiKey}";
+            var client = new WebClient();
+
+            var body = client.DownloadString(url);
+
+            JObject data = JObject.Parse(body);
+            int mainTempInCelsius = (int)(data["main"]["temp"]);
+            double visibilityInKm = (double)data["visibility"] / 1000;
+            double windSpeed = (double)data["wind"]["speed"];
+            //for test
+            windSpeed = 15;
+
+
+            ViewData["temp"] = mainTempInCelsius;
+            ViewData["city"] = data["name"];
+            ViewData["country"] = data["sys"]["country"];
+            ViewData["weather"] = data["weather"][0]["main"];
+            ViewData["humidity"] = data["main"]["humidity"];
+            ViewData["wind"] = windSpeed;
+            ViewData["visibility"] = visibilityInKm;
+            ViewData["atmPressure"] = data["main"]["pressure"];
+
+            // var droneWindResist = dronesWindResist[droneModel];
+            double droneWindResist = dronesWindResist.Where(x => x.Key == selectedDroneModel).FirstOrDefault().Value;
+
+            if (droneWindResist <= windSpeed)
+            {
+                ViewData["alert"] = $"Fly with caution! Your drone has wind resist of" +
+                    $" {droneWindResist} m/s but the wind in the region is {windSpeed} m/s";
+                ViewData["alertLevel"] = "caution";
+            }
+            else
+            {
+                ViewData["alert"] = "Flying your drone is completely safe!";
+                ViewData["alertLevel"] = "missing";
+            }
+
+            
+
             return View();
         }
 
 
-            /// <summary>
-            /// This will be used for post method which will capture input of the user(name of the city)
-            /// and translate it to geolocation cordinates
-            /// </summary>
-            /// <param name="city"></param>
-            /// <returns>  List<string> with 2 params - lat and lon </returns>
-            private List<string> FetchGeolocation(string city)
+        /// <summary>
+        /// This will be used for post method which will capture input of the user(name of the city)
+        /// and translate it to geolocation cordinates
+        /// </summary>
+        /// <param name="city"></param>
+        /// <returns>  List<string> with 2 params - lat and lon </returns>
+        private List<string> FetchGeolocation(string city)
         {
             var url = $"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={_ApiKey}";
             var client = new WebClient();
             var body = client.DownloadString(url);
             JArray data = JArray.Parse(body);
-
-            var info1 = (Math.Round((double)data[0]["lat"], 2));
-            var info2 = (Math.Round((double)data[0]["lon"], 2));
-
             List<string> geolocations = new List<string>();
-            geolocations.Add(info1.ToString());
-            geolocations.Add(info2.ToString());
+
+            if (data.HasValues)
+            {
+                var info1 = (Math.Round((double)data[0]["lat"], 2));
+                var info2 = (Math.Round((double)data[0]["lon"], 2));
+                geolocations.Add(info1.ToString());
+                geolocations.Add(info2.ToString());
+            }
+
+
 
 
             return geolocations;
@@ -152,7 +219,7 @@ namespace DroneWeatherAnalyzer.Controllers
             //to be sure I trim the result from errors after
             capitalCityName.Trim();
             return capitalCityName;
-            
+
         }
     }
 }
